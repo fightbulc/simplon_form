6307f53ac06191cfd839beddf3eda9b0f3bff9ff
Tino Ehrich <ehrich@efides.com>
2012-11-07 14:05:20 +0100

    implement checked state

--- src/Simplon/Form/Elements/CheckboxField.php
+++ src/Simplon/Form/Elements/CheckboxField.php
@@ -11,4 +11,60 @@
     {
       return 'checkbox';
     }
+
+    // ##########################################
+
+    /**
+     * @param $value
+     * @return AbstractElement|CheckboxField
+     */
+    public function setValue($value)
+    {
+      if($value !== FALSE)
+      {
+        parent::setValue($value);
+        $this->setChecked();
+      }
+
+      return $this;
+    }
+
+    // ##########################################
+
+    /**
+     * @return CheckboxField
+     */
+    public function setChecked()
+    {
+      $this->_setByKey('checked', 'checked');
+
+      return $this;
+    }
+
+    // ##########################################
+
+    /**
+     * @return bool|mixed
+     */
+    public function getChecked()
+    {
+      return $this->_getByKey('checked');
+    }
+
+    // ##########################################
+
+    /**
+     * @return array
+     */
+    protected function _getElementAttributes()
+    {
+      return array(
+        'type'    => $this->getType(),
+        'id'      => $this->getId(),
+        'name'    => $this->getId(),
+        'class'   => $this->getClass(),
+        'value'   => $this->getValue(),
+        'checked' => $this->getChecked(),
+      );
+    }
   }
